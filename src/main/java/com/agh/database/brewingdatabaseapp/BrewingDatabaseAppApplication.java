617d2782f52c24b3b624250b72d18cd9812f62dd
Przemyslaw5 <przemyslaw.watroba@gmail.com>
2020-05-08 17:23:13 +0200

    Initial Project Add

---
+++ src/main/java/com/agh/database/brewingdatabaseapp/BrewingDatabaseAppApplication.java
@@ -0,0 +1,12 @@
+package com.agh.database.brewingdatabaseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewingDatabaseAppApplication {
+
+    public static void main(String[] args) { SpringApplication.run(BrewingDatabaseAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agh/database/brewingdatabaseapp/BrewingDatabaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.database.brewingdatabaseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewingDatabaseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

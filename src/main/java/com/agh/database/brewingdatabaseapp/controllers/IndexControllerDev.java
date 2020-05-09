cacd0c2ea5c0fa1ab560cb64b485083db1b42918
Przemyslaw5 <przemyslaw.watroba@gmail.com>
2020-05-09 16:48:53 +0200

    Add Profiles in Controller

---
+++ src/main/java/com/agh/database/brewingdatabaseapp/controllers/IndexControllerDev.java
@@ -0,0 +1,24 @@
+package com.agh.database.brewingdatabaseapp.controllers;
+
+import com.agh.database.brewingdatabaseapp.services.BatchService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Profile("dev")
+public class IndexControllerDev {
+
+    private final BatchService batchService;
+
+    public IndexControllerDev(BatchService batchService) {
+        this.batchService = batchService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showOneBatch(Model model){
+        model.addAttribute("batch", batchService.findById("5eb6a8d9c2f1ab25a71ff3fc"));
+        return "index";
+    }
+}
--- src/main/java/com/agh/database/brewingdatabaseapp/controllers/IndexControllerProd.java
+++ src/main/java/com/agh/database/brewingdatabaseapp/controllers/IndexControllerProd.java
@@ -1,22 +1,24 @@
 package com.agh.database.brewingdatabaseapp.controllers;
 
 import com.agh.database.brewingdatabaseapp.services.BatchService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+@Profile("prod")
+public class IndexControllerProd {
 
     private final BatchService batchService;
 
-    public IndexController(BatchService batchService) {
+    public IndexControllerProd(BatchService batchService) {
         this.batchService = batchService;
     }
 
     @RequestMapping({"/", ""})
     public String showOneBatch(Model model){
-        model.addAttribute("batch", batchService.findById("5eb6a8d9c2f1ab25a71ff3fc"));
+        model.addAttribute("batch", batchService.findById("5eb6598ac4afbc5dccd17ee4"));
         return "index";
     }
 }
